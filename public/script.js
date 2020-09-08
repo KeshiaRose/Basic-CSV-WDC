e7eb744fe91ed000d2520e04031c497aa7d4954c
Glitch (simple-csv-wdc) <none>
2020-09-08 19:08:15 +0000

    🙎📻 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -1,6 +1,9 @@
 /* global tableau Papa */
 
-// Test URL: https://en.unesco.org/sites/default/files/covid_impact_education.csv
+// Test URLs
+// https://en.unesco.org/sites/default/files/covid_impact_education.csv
+// https://opendata.ecdc.europa.eu/covid19/casedistribution/csv
+// https://data.cityofnewyork.us/resource/ic3t-wcy2.csv
 
 let cachedTableData; // Always a JSON object
 
@@ -17,22 +20,21 @@ myConnector.getSchema = async function(schemaCallback) {
   let data =
     cachedTableData || (await _retrieveCSVData({ dataUrl, method, token }));
 
-    let cols = [];
+  let cols = [];
 
-    for (let field in data.headers) {
-      cols.push({
-        id: field,
-        alias: data.headers[field].alias,
-        dataType: data.headers[field].dataType
-      });
-    }
+  for (let field in data.headers) {
+    cols.push({
+      id: field,
+      alias: data.headers[field].alias,
+      dataType: data.headers[field].dataType
+    });
+  }
 
-    let tableSchema = {
-      id: "csvData",
-      alias: "CSV Data",
-      columns: cols
-    };
-  console.log("Table schema created.");
+  let tableSchema = {
+    id: "csvData",
+    alias: "CSV Data",
+    columns: cols
+  };
 
   schemaCallback([tableSchema]);
 };
@@ -64,11 +66,11 @@ function _submitDataToTableau() {
   let method = $("#method").val();
   let token = $("#token").val();
   if (!dataUrl) return _error("No data entered.");
-  
+
   const urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
   const result = dataUrl.match(urlRegex);
   if (result === null) return _error("URL is not valid.");
-  
+
   tableau.connectionData = JSON.stringify({ dataUrl, method });
   tableau.password = token;
 
@@ -117,44 +119,52 @@ function _csv2table(csv) {
 
     headers[newKey] = { alias: field };
   }
-
+  let counts = lines.map(line => line.length);
+  let lineLength = counts.reduce((m, c) =>
+    counts.filter(v => v === c).length > m ? c : m
+  );
+  
   for (let line of lines) {
-    let obj = {};
-    let headerKeys = Object.keys(headers);
-    for (let field in headerKeys) {
-      let header = headers[headerKeys[field]];
-      let value = line[field];
+    if (line.length === lineLength) {
+      let obj = {};
+      let headerKeys = Object.keys(headers);
+      for (let field in headerKeys) {
+        let header = headers[headerKeys[field]];
+        let value = line[field];
 
-      if (
-        value === "" ||
-        value === '""' ||
-        value === "null" ||
-        value === null
-      ) {
-        obj[headerKeys[field]] = null;
-        header.null = header.null ? header.null + 1 : 1;
-      } else if (value === "true" || value === true) {
-        obj[headerKeys[field]] = true;
-        header.bool = header.bool ? header.bool + 1 : 1;
-      } else if (value === "false" || value === false) {
-        obj[headerKeys[field]] = false;
-        header.bool = header.bool ? header.bool + 1 : 1;
-      } else if (typeof value === "object") {
-        obj[headerKeys[field]] = value.toISOString();
-        header.string = header.string ? header.string + 1 : 1;
-      } else if (!isNaN(value)) {
-        obj[headerKeys[field]] = value;
-        if (parseInt(value) == value) {
-          header.int = header.int ? header.int + 1 : 1;
+        if (
+          value === "" ||
+          value === '""' ||
+          value === "null" ||
+          value === null
+        ) {
+          obj[headerKeys[field]] = null;
+          header.null = header.null ? header.null + 1 : 1;
+        } else if (value === "true" || value === true) {
+          obj[headerKeys[field]] = true;
+          header.bool = header.bool ? header.bool + 1 : 1;
+        } else if (value === "false" || value === false) {
+          obj[headerKeys[field]] = false;
+          header.bool = header.bool ? header.bool + 1 : 1;
+        } else if (typeof value === "object") {
+          obj[headerKeys[field]] = value.toISOString();
+          header.string = header.string ? header.string + 1 : 1;
+        } else if (!isNaN(value)) {
+          obj[headerKeys[field]] = value;
+          if (parseInt(value) == value) {
+            header.int = header.int ? header.int + 1 : 1;
+          } else {
+            header.float = header.float ? header.float + 1 : 1;
+          }
         } else {
-          header.float = header.float ? header.float + 1 : 1;
+          obj[headerKeys[field]] = value;
+          header.string = header.string ? header.string + 1 : 1;
         }
-      } else {
-        obj[headerKeys[field]] = value;
-        header.string = header.string ? header.string + 1 : 1;
       }
+      rows.push(obj);
+    } else {
+      console.log("Row ommited due to mismatched length.", line)
     }
-    rows.push(obj);
   }
 
   for (let field in headers) {
