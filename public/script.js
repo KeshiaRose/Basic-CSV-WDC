4963174f9a817f290774c2147ca74a1198dbe96e
Glitch (simple-csv-wdc) <none>
2021-02-11 17:58:22 +0000

    🙆🌑 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -96,7 +96,7 @@ tableau.connectionName = "CSV Data";
 tableau.registerConnector(myConnector);
 
 // Grabs wanted fields and submits data to Tableau
-function _submitDataToTableau() {
+async function _submitDataToTableau() {
   let dataUrl = $("#url")
     .val()
     .trim();
@@ -104,11 +104,13 @@ function _submitDataToTableau() {
   let token = $("#token").val();
   let delimiter = $("#delimiter").val();
   if (!dataUrl) return _error("No data entered.");
-
-  // const urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+  
   const urlRegex = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/|ftp:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
   const result = dataUrl.match(urlRegex);
-  if (result === null) return _error("URL is not valid.");
+  if (result === null) {
+    _error("WARNING: URL may not be valid...");
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
 
   tableau.connectionData = JSON.stringify({ dataUrl, method, delimiter });
   tableau.password = token;
